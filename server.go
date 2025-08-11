da9f3c6007834d72229edc1bb4eb575201919deb
Simon Cardona <simon.cdna@proton.me>
2025-08-11 14:46:39 +0200

    feat(server): add healthz handler

--- server.go
+++ server.go
@@ -14,18 +14,26 @@ func server() error {
 	fileServer := http.FileServer(http.Dir(filepathRoot))
 
 	mux := http.NewServeMux()
-	mux.Handle("/", fileServer)
+
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	fmt.Printf("serving on port: %s", port)
+	fmt.Printf("serving on port: %s\n", port)
 	err := server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("error on serving: %w", err)
 	}
 
 	return nil
+}
+
+func healthzHandler (res http.ResponseWriter, _ *http.Request) {	
+		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte("OK"))
 }
